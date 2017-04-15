d0ad74fb6edf9384fea14549a9eaefad8ca3f0d6
animavitis <rherok@gmail.com>
2017-04-15 22:51:36 +0200

    update example

--- examples/ShowReceivedCode_ESP8266/ShowReceivedCode_ESP8266.ino
+++ examples/ShowReceivedCode_ESP8266/ShowReceivedCode_ESP8266.ino
@@ -9,32 +9,34 @@
 #include <NewRemoteReceiver.h>
 
 void setup() {
-  Serial.begin(9600);
-  
+  Serial.begin(115200);
+
   // Initialize receiver on interrupt 0 (= digital pin 2) for arduino uno, calls the callback "showCode"
   // after 1 identical codes have been received in a row. (thus, keep the button pressed
   // for a moment), on esp8266 use on interrupt 5 = digital pin 1
   //
   // See the interrupt-parameter of attachInterrupt for possible values (and pins)
   // to connect the receiver.
-  NewRemoteReceiver::init(0, 1, showCode);
+  // NewRemoteReceiver::init(5, 0, showCode);
 }
 
 void loop() {
 }
 
 // Callback function is called only when a valid code is received.
-void showCode(unsigned int period, unsigned long address) {
+void showCode(unsigned int period, unsigned long address, unsigned long groupBit, unsigned long unit, unsigned long switchType) {
   // Note: interrupts are disabled. You can re-enable them if needed.
-  
+
   // Print the received code.
   Serial.print("Code: ");
   Serial.print(address);
   Serial.print(" Period: ");
   Serial.println(period);
-  /*Serial.print(", period duration: ");
-  Serial.print(period);
-  Serial.println("us.");*/
+  Serial.print(" unit: ");
+  Serial.println(unit);
+  Serial.print(" groupBit: ");
+  Serial.println(groupBit);
+  Serial.print(" switchType: ");
+  Serial.println(switchType);
+
 }
-
-
